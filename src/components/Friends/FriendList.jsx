5cdc2c35b2c97e85d2dc0916e05b504417e382f1
guemplen <guemplen@gmail.com>
2024-04-29 20:15:17 +0200

    init repo
    Add task1, task2, task3

---
+++ src/components/Friends/FriendList.jsx
@@ -0,0 +1,24 @@
+import styles from './friend.module.css'
+
+const FriendList = ({ friends }) => (
+  <ul className={styles.list}>
+    {friends.map(friend => (
+      <li key={friend.id} className={styles.item}>
+        <div className={styles.avatar}>
+          <img src={friend.avatar} alt="User avatar" />
+        </div>
+        <div className={styles.info}>
+          <p className={styles.name}>{friend.name}</p>
+          <span
+            className="status"
+            style={{ color: friend.isOnline ? 'green' : 'red' }}
+          >
+            {friend.isOnline ? 'Online' : 'Offline'}
+          </span>
+        </div>
+      </li>
+    ))}
+  </ul>
+);
+
+export default FriendList;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,30 @@
+import styles from './profile.module.css';
+
+function Profile({ username, tag, location, image, stats }) {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={image} alt="User image" className={styles.image} />
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+      <ul className={styles.stats}>
+        <li>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Profile;
